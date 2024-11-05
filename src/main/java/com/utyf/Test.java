b4420b70e7b87da9ac7f219d246aa9b35d08300e
Utyf <utyff@mail.ru>
2024-11-05 23:02:30 +0300

    update

--- src/main/java/com/utyf/Test.java
+++ src/main/java/com/utyf/Test.java
@@ -1,7 +1,10 @@
 package com.utyf;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by adm on 08.10.2016.
